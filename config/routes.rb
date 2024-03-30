a932c2b2d8c0b43032a556561a063921b0253770
Somya Abbar <somya.abbar@gmail.com>
2024-03-30 22:08:52 +0800

    adding css to home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/about', to: 'about#show'
+  get '/cambodia', to: 'pages#cambodia'
+  get '/roadtrip', to: 'pages#roadtrip'
+  get '/index', to: 'pages#index'
+  get '/southeastasia', to: 'pages#southeastasia'
+  get '/middleeast', to: 'pages#middleeast'
+  get '/eastasia', to: 'pages#eastasia'
+  get '/thailand', to: 'pages#thailand'
+  get '/philippines', to: 'pages#philippines'
+  get '/indonesia', to: 'pages#indonesia'
+  get '/malaysia', to: 'pages#malaysia'
+  get '/vietnam', to: 'pages#vietnam'
+  get '/japan', to: 'pages#japan'
+  get '/southkorea', to: 'pages#southkorea'
 end
